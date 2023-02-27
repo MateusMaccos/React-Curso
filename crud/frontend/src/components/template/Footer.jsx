993c423acb9512041ce0405ab5a92244916ac926
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-02-27 12:00:52 -0300

    Cadastro de Usuarios

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desevolvido com <i className='fa fa-heart text-danger'></i> por
+            <strong> Cartoon<span className='text-danger'>Matt</span></strong>
+        </span>
+    </footer>
